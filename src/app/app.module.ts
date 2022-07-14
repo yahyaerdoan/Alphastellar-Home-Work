8cca22f379fa9ab0cd71d3a6a928726899cb3d77
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-07-14 17:48:13 +0300

    Proje ve sınıfları oluşturuldu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/headers/header/header.component';
+import { FooterComponent } from './components/footers/footer/footer.component';
+import { SideBarComponent } from './components/side-bars/side-bar/side-bar.component';
+import { BodyComponent } from './components/bodies/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideBarComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
